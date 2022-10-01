9ec0427f2acbda7985671f7272a80d9163dd294b
abhiktr <113281196+abhiktr@users.noreply.github.com>
2022-10-01 13:44:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from urllib import response
+import requests
+
+USERNAME = "abhishek"
+TOKEN = "asdfghjkl;jjgshgba"
+GRAPH_ID = "graph1"
+
+requests.post()
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_param = {
+    "token": TOKEN,
+    "username":USERNAME,
+    "agreeTermsOfService":"yes",
+    "notMinor": "yes"
+}
+
+# response=requests.post(url=pixela_endpoint,json=user_param)
+# print(response.text)
+
+graph_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config={
+    "id":GRAPH_ID,
+    "name":"Coding Graph",
+    "unit":"hours",
+    "types":"float",
+    "color":"sora"
+    
+}
+
+headers = {
+    "X-USER-TOKEN":TOKEN
+}
+
+# response = requests.post(url=graph_endpoint,json=graph_config,headers=headers)
+# print(response.text)
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today=datetime.now()
+
+pixel_data={
+    "date": today.strftime("%Y%m%d"),
+    "quantity":"2.3"
+}
+
+response=requests.post(url=pixel_creation_endpoint,json=pixel_data,headers=headers)
+print(response.text)
+
